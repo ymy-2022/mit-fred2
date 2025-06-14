8951ce375622ca02fee3446a40bf9d7bda0ef407
ymy-2022 <97040839+ymy-2022@users.noreply.github.com>
2025-06-14 13:12:27 -0400

    Update user_interface.py

--- user_interface.py
+++ user_interface.py
@@ -137,7 +137,7 @@ class UserInterface:
             self.figure = Figure()
             self.axes = self.figure.add_subplot(111)
             super().__init__(self.figure)
-            self.figure.subplots_adjust(top=0.88)  # 裁掉title上方白边
+            self.figure.subplots_adjust(top=0.8)  # 裁掉title上方白边
             self.axes.set_title(title)
             self.axes.set_xlabel("Time (s)")
             self.axes.set_ylabel(y_label)
